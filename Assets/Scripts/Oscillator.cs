2169604632fbf85d8b95aad2185f14f04ae066fa
Matija Benko <58277234+MatijaBenko@users.noreply.github.com>
2025-03-09 20:44:16 -0700

    Add changes

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,18 +4,25 @@ public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector3 movementVector = new Vector3(0f, 0f, 0f);
     [SerializeField] float speed = 2f;
+    [SerializeField] float delay = 0f;
     float movementFactor = 0f;
     Vector3 startPosition;
     Vector3 endPosition;
+    float timer = 0f;
 
     void Start()
     {
         startPosition = transform.position;
         endPosition = startPosition + movementVector;
     }
+
     void Update()
     {
-        movementFactor = Mathf.PingPong(Time.time * speed, 1f);
-        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+        timer += Time.deltaTime;
+        if (timer >= delay)
+        {
+            movementFactor = Mathf.PingPong((timer - delay) * speed, 1f);
+            transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+        }
     }
 }
